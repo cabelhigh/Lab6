886230c26f1ccbbc3b4d377869581e81644fcee7
rwongoberlin <rwong@oberlin.edu>
2013-11-01 17:32:45 -0400

    Update format.c

--- format.c
+++ format.c
@@ -1,29 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "format.h"
+//adds poiter references to the front?
+// need to figure out how to properly go to the next line
 
-FILE *fp;
 
+char* string;
 int width = 72;
 
 int main(int argc, char *argv[]){
 
-	for(int i = 0; i < argc; i++){
-		if(argv[i][0]=='-'){
-			if(argv[i][1]=='w'){
-				i++;				
-				width = atoi(argv[i]);
-			}
-			else if(argv[i][1]=='r') {
-		}
-	}
-		else{
-			fp = fopen(argv[i], "r");
-			if(fp){}
-			else{
-				printf("Sorry, can't open file %s!", argv[i]);
-			}
-		}
-	}
-	
+        for(int i = 1; i < argc; i++){
+                if(argv[i][0]=='-'){
+                        if(argv[i][1]=='w'){
+                                i++;
+                                width = atoi(argv[i]);
+                        }
+                        else if(argv[i][1]=='r') {
+                        }
+                }
+                else{
+                        char line[width];
+                        string = argv[i];
+//if less than width add to the current line
+                        if(strlen(line)+strlen(string)<=width ) {
+        //copy string into line
+                        *line=strcat(line, string);
+
+                //      *line=' ';
+        }
+//                        printf("%s\n", string);
+                        printf("%s\n", line);
+
 }
-							
+
+
+}
+}
